ed21b2e67b36378579225ee78791e29895baa313
Robert Lowe <robertmlowe@rmlowe.com>
2018-12-23 00:29:57 +0800

    Step 34 - Implementing a GET service to retrieve all Posts of a User

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserJPAResource.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserJPAResource.java
@@ -77,4 +77,15 @@ public class UserJPAResource {
 		
 	}
 
+	@GetMapping("/jpa/users/{id}/posts")
+	public List<Post> retrieveAllUsers(@PathVariable int id) {
+		Optional<User> userOptional = userRepository.findById(id);
+		
+		if(!userOptional.isPresent()) {
+			throw new UserNotFoundException("id-" + id);
+		}
+		
+		return userOptional.get().getPosts();
+	}
+
 }
